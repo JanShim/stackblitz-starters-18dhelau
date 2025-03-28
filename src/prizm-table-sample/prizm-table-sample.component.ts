a893ef3fa7eef7b04f0ab45fc7ecc1d40868b75f
JanShim <janshimkus@gmail.com>
2025-03-28 17:11:38 +0300

    feat: init app

---
+++ src/prizm-table-sample/prizm-table-sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prizm-table-sample',
+  standalone: true,
+  imports: [],
+  templateUrl: './prizm-table-sample.component.html',
+  styleUrl: './prizm-table-sample.component.css'
+})
+export class PrizmTableSampleComponent {
+
+}
